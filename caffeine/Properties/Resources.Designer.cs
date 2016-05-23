47c0ddd146cde1b3b828ef51a21c7272e72e169b
Bryan Jaudon <bryan.jaudon@gmail.com>
2016-05-23 15:50:17 -0400

    Change application icon to black icon version.

--- caffeine/Properties/Resources.Designer.cs
+++ caffeine/Properties/Resources.Designer.cs
@@ -131,6 +131,16 @@ namespace Caffeine.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
         /// </summary>
+        internal static System.Drawing.Icon caffeineNew_Black {
+            get {
+                object obj = ResourceManager.GetObject("caffeineNew_Black", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
         internal static System.Drawing.Icon caffeineNewIcon {
             get {
                 object obj = ResourceManager.GetObject("caffeineNewIcon", resourceCulture);
