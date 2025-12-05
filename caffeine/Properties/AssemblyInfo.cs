e96ed6c4dd59368ea0a088a6279e1e02841b0443
bjaudon <bryan.jaudon@gmail.com>
2025-12-05 17:35:41 -0500

    Store final changes for step 'Upgrade Caffeine\Caffeine.csproj'

--- caffeine/Properties/AssemblyInfo.cs
+++ caffeine/Properties/AssemblyInfo.cs
@@ -29,9 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
+// Use explicit version numbers - wildcards are not supported in deterministic builds.
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: NeutralResourcesLanguage("en")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
